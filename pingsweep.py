ad232293df3b6dad995c68c38666dc0bebbd91aa
odaah <vijay.emarose@gmail.com>
2017-04-14 20:43:14 -0500

    Update pingsweep.py

--- pingsweep.py
+++ pingsweep.py
@@ -8,7 +8,7 @@ net_split = net.rsplit(".",1)
 range_start = net_split[1].split("-")[0]
 range_end = net_split[1].split("-")[1]
 
-with open(os.devnull, "wb") as limbo:
+with open(os.devnull, os.O_RDWR) as limbo:
         for n in xrange(int(range_start),int(range_end)):
                 ip=net_split[0]+"."+format(n)
                 result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", ip],
